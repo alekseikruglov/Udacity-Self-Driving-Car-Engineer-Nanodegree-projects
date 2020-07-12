628af28c29212215798198fc9437e80b536bf53c
Aleksei Kruglov <aleksei.kruglov.de@gmail.com>
2020-07-12 10:20:54 +0200

    Add Project 5 Extended Kalman Filter

---
+++ P5_Extended_Kalman_Filter/README.md
@@ -0,0 +1,94 @@
+# Extended Kalman Filter Project Starter Code
+Udacity Self-Driving Car Engineer Nanodegree Program
+
+## Overview
+
+In this project an Extended Kalman Filter was utilized in C++ to estimate the state of a moving object of interest using Sensor Fusion of noisy LIDAR and RADAR measurements. 
+
+<p align="center">
+  <img src="./README_images/gif_video.gif" alt = "Kalman filter visualization" width = 600 />
+</p>
+
+<p align="center">
+  <img src="./README_images/video_legend.png" alt = "Kalman filter visualization" width = 140 />
+</p>
+
+
+The Kalman Filter predicts the objects's state after a time interval using a mathematical model and clarifies the state using the measurement information. 
+
+<p align="center">
+  <img src="./README_images/predict_update.png" alt = "Kalman filter process" width = 600 />
+</p>
+
+In this project the measurement data is in 2D, so four states are estimated by Kalman Filter: x-position, y-position, x-velocity, y-velocity:
+
+<p align="center">
+  <img src="./README_images/kf_prediction.png" alt = "Kalman filter states" width = 600 />
+</p>
+
+
+Sensor fusion combines LIDAR and RADAR data together to make accurate object tracking with Extended Kalman Filter (EKF):
+
+<p align="center">
+  <img src="./README_images/plan.png" alt = "Kalman filter general process" width = 600 />
+</p>
+
+
+
+## Dependencies
+
+This project involves the Term 2 Simulator which can be downloaded [here](https://github.com/udacity/self-driving-car-sim/releases).
+
+This repository includes two files that can be used to set up and install [uWebSocketIO](https://github.com/uWebSockets/uWebSockets) for either Linux or Mac systems. For windows you can use either Docker, VMware, or even [Windows 10 Bash on Ubuntu](https://www.howtogeek.com/249966/how-to-install-and-use-the-linux-bash-shell-on-windows-10/) to install uWebSocketIO. Please see the uWebSocketIO Starter Guide page in the classroom within the EKF Project lesson for the required version and installation scripts.
+
+
+### Other Important Dependencies
+
+* cmake >= 3.5
+  * All OSes: [click here for installation instructions](https://cmake.org/install/)
+* make >= 4.1 (Linux, Mac), 3.81 (Windows)
+  * Linux: make is installed by default on most Linux distros
+  * Mac: [install Xcode command line tools to get make](https://developer.apple.com/xcode/features/)
+  * Windows: [Click here for installation instructions](http://gnuwin32.sourceforge.net/packages/make.htm)
+* gcc/g++ >= 5.4
+  * Linux: gcc / g++ is installed by default on most Linux distros
+  * Mac: same deal as make - [install Xcode command line tools](https://developer.apple.com/xcode/features/)
+  * Windows: recommend using [MinGW](http://www.mingw.org/)
+
+## Basic Build Instructions
+
+1. Clone this repo.
+2. Make a build directory: `mkdir build && cd build`
+3. Compile: `cmake .. && make` 
+   * On windows, you may need to run: `cmake .. -G "Unix Makefiles" && make`
+4. Run it: `./ExtendedKF `
+
+
+## File overview
+
+* main.cpp:
+  * Contains interface to Udacity Simulator
+* FusionEKF.cpp:
+  * Initializes Kalman Filter variables for RADAR and LIDAR data
+  * Calls predict and update functions
+* kalman_filter.cpp:
+  * Contains Extended Klaman Filter algorithms for RADAR and LIDAR
+* tools.cpp
+  * Contains functions to calculate RMSE and Jacobian matrix
+## Results
+
+The resulting Extended Kalman Filter estimations (green) are almost the same as ground truth (blue) in the image below. The visualization was made with [Sensor Fusion Toolkit](https://github.com/udacity/CarND-Mercedes-SF-Utilities) provided by Mercedes team.
+
+
+<p align="center">
+  <img src="./README_images/full_way.png" alt = "Full way" width = 800 />
+</p>
+
+The Extended Kalman Filter has following RMSE in position and velocity components:
+
+|    | RMSE |
+|----|------|
+| px | 0.0974     |
+| py | 0.0855     |
+| vx | 0.4517     |
+| vy | 0.4404     |
---
+++ P5_Extended_Kalman_Filter/src/FusionEKF.cpp
@@ -0,0 +1,194 @@
+#include "FusionEKF.h"
+#include <iostream>
+#include "Eigen/Dense"
+#include "tools.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using std::cout;
+using std::endl;
+using std::vector;
+
+/**
+ * Constructor.
+ */
+FusionEKF::FusionEKF() {
+  is_initialized_ = false;
+
+  previous_timestamp_ = 0;
+
+  // initializing matrices
+  R_laser_ = MatrixXd(2, 2);
+  R_radar_ = MatrixXd(3, 3);
+  H_laser_ = MatrixXd(2, 4);
+  Hj_ = MatrixXd(3, 4);
+
+  //measurement covariance matrix - laser
+  R_laser_ << 0.0225, 0,
+              0, 0.0225;
+
+  //measurement covariance matrix - radar
+  R_radar_ << 0.09, 0, 0,
+              0, 0.0009, 0,
+              0, 0, 0.09;
+
+  /**
+   * TODO: Finish initializing the FusionEKF.
+   * TODO: Set the process and measurement noises
+   */
+
+  //state covariance matrix P
+	MatrixXd P_ = MatrixXd(4, 4); 
+  
+	P_ << 1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1000, 0,
+        0, 0, 0, 1000;
+
+  H_laser_ << 1, 0, 0, 0,
+            0, 1, 0, 0;
+
+  Hj_ << 0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0;
+  
+  //the initial transition matrix F_
+	MatrixXd F_ = MatrixXd(4, 4);
+	F_ << 1, 0, 1, 0,
+        0, 1, 0, 1,
+        0, 0, 1, 0,
+        0, 0, 0, 1;
+
+  // process convariance matrix
+	MatrixXd Q_ = MatrixXd(4, 4);
+	Q_ << 1, 0, 1, 0,
+				0, 1, 0, 1,
+				1, 0, 1, 0,
+				0, 1, 0, 1;
+
+
+  VectorXd x_ = VectorXd(4);
+	x_ << 0, 0, 0, 0;
+  ekf_.Init(x_, P_, F_, H_laser_, R_laser_, Q_);
+
+}
+
+/**
+ * Destructor.
+ */
+FusionEKF::~FusionEKF() {}
+
+void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
+  /**
+   * Initialization
+   */
+  if (!is_initialized_) {
+    /**
+     * TODO: Initialize the state ekf_.x_ with the first measurement.
+     * TODO: Create the covariance matrix.
+     * You'll need to convert radar from polar to cartesian coordinates.
+     */
+ 
+    float px = 0.0;
+    float py = 0.0;
+    float vx = 0.0;
+    float vy = 0.0;
+
+    if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+      // TODO: Convert radar from polar to cartesian coordinates 
+      //         and initialize state.
+
+      //polar
+      float ro = measurement_pack.raw_measurements_[0];
+      float fi = measurement_pack.raw_measurements_[1];
+      float rod = measurement_pack.raw_measurements_[2];
+
+      //cartesian
+      px = ro*cos(fi);
+      py = ro*sin(fi);
+      // no speed by first measurement -> speed = 0
+
+    }
+    else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
+      // TODO: Initialize state.
+      
+      //first measurement -> speed = 0
+      px = measurement_pack.raw_measurements_[0];
+      py = measurement_pack.raw_measurements_[1];
+
+    }
+
+    //initialize state
+    ekf_.x_ << px, py, vx, vy;
+
+    //update timestamp
+    previous_timestamp_ = measurement_pack.timestamp_;
+
+    // done initializing, no need to predict or update
+    is_initialized_ = true;
+    return;
+  }
+
+  /**
+   * Prediction
+   */
+
+  /**
+   * TODO: Update the state transition matrix F according to the new elapsed time.
+   * Time is measured in seconds.
+   * TODO: Update the process noise covariance matrix.
+   * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
+   */
+
+  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;  //microseconds -> seconds
+  //Update timestamp
+  previous_timestamp_ = measurement_pack.timestamp_;
+
+  ekf_.F_(0,2) = dt;
+  ekf_.F_(1,3) = dt;
+
+  float dt_2 = dt * dt;
+  float dt_3 = dt_2 * dt;
+  float dt_4 = dt_3 * dt;
+
+  float noise_ax = 9.0;
+  float noise_ay = 9.0;
+
+  ekf_.Q_ <<  dt_4/4.0*noise_ax, 0, dt_3/2.0*noise_ax, 0,
+              0, dt_4/4.0*noise_ay, 0, dt_3/2.0*noise_ay,
+              dt_3/2.0*noise_ax, 0, dt_2*noise_ax, 0,
+              0, dt_3/2.0*noise_ay, 0, dt_2*noise_ay;
+
+  ekf_.Predict();
+
+  /**
+   * Update
+   */
+
+  /**
+   * TODO:
+   * - Use the sensor type to perform the update step.
+   * - Update the state and covariance matrices.
+   */
+
+  
+  if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+    // TODO: Radar updates
+
+    ekf_.R_ = R_radar_;
+    ekf_.H_ = tools.CalculateJacobian(ekf_.x_); //Radar!!! -> calculate Jacobian!
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
+    //cout << "Update radar: " << endl;
+  } else {
+    // TODO: Laser updates
+
+    ekf_.R_ = R_laser_;
+    ekf_.H_ = H_laser_;
+    ekf_.Update(measurement_pack.raw_measurements_);
+    //cout << "Update laser: " << endl;
+  }
+
+  // print the output
+  cout << "x_ = " << ekf_.x_ << endl;
+  cout << "P_ = " << ekf_.P_ << endl;
+}
